b6adc843e26213d3d54fabc27b631f0cdfe803bd
wangfei <wangfei@aidigger.com>
2021-03-10 18:58:56 +0800

    cobra test

--- cmd/root.go
+++ cmd/root.go
@@ -65,12 +65,14 @@ func init() {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
+	fmt.Println("I'm inside initConfig function in cmd/root.go")
 	if cfgFile != "" {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
 		// Find home directory.
 		home, err := homedir.Dir()
+		fmt.Println("home:",home)
 		cobra.CheckErr(err)
 
 		// Search config in home directory with name ".athena" (without extension).
@@ -82,6 +84,7 @@ func initConfig() {
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("hello abc")
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
 }
